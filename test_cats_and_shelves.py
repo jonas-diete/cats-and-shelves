0ebef863a9075a51540a72110c3cd23dd3b93cda
Jonas Diete <jonas.diete@gmail.com>
2023-04-17 14:34:33 +0100

    three passing tests for same level start finish

--- test_cats_and_shelves.py
+++ test_cats_and_shelves.py
@@ -2,3 +2,9 @@ from cats_and_shelves import cats_and_shelves
 
 def test_start_and_finish_one_returns_zero():
     assert cats_and_shelves(1, 1) == 0
+
+def test_start_and_finish_three_returns_zero():
+    assert cats_and_shelves(3, 3) == 0
+
+def test_start_and_finish_twentyfour_returns_zero():
+    assert cats_and_shelves(24, 24) == 0
