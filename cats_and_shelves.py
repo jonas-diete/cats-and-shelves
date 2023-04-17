2cba59233dc42c6c85bed22e705cd6f0e483cd8f
Jonas Diete <jonas.diete@gmail.com>
2023-04-17 14:38:31 +0100

    start and finish one apart tests pass

--- cats_and_shelves.py
+++ cats_and_shelves.py
@@ -1,3 +1,5 @@
 def cats_and_shelves(start_shelf, finish_shelf):
     if start_shelf == finish_shelf:
         return 0
+    else:
+        return 1
--- test_cats_and_shelves.py
+++ test_cats_and_shelves.py
@@ -8,3 +8,12 @@ def test_start_and_finish_three_returns_zero():
 
 def test_start_and_finish_twentyfour_returns_zero():
     assert cats_and_shelves(24, 24) == 0
+
+def test_start_and_finish_one_apart_returns_one1():
+    assert cats_and_shelves(1, 2) == 1
+
+def test_start_and_finish_one_apart_returns_one2():
+    assert cats_and_shelves(5, 6) == 1
+
+def test_start_and_finish_one_apart_returns_one3():
+    assert cats_and_shelves(24, 25) == 1
